2db51221c5c8c5f5e5a08c489a798df73ec967a6
ScaerieTale <7010800+ScaerieTale@users.noreply.github.com>
2022-04-04 20:43:09 -0500

    Hubris is a b**** :)
    I thought I was finished, but she threw me a curve ball, wanting both functions combined into one.
    
    NOW I'm finished lol

--- main.py
+++ main.py
@@ -19,29 +19,16 @@ shift = int(input("Type the shift number:\n"))
     ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
 #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
-def encrypt(userInput, shftAmt):
-    # I had to move newMsg out of the for loop
-    # because otherwise it would just generate one character
-    # instead of adding each character in sequence
+def Caesar(choice, userInput, shftAmt):
     newMsg = ''
     for letter in userInput:
         letPos = alphabet.index(letter)
-        newPos = letPos + shftAmt
+        if choice == "encode":
+            newPos = letPos + shftAmt
+        else:
+            newPos = letPos - shift
         newLet = alphabet[newPos]
         newMsg += newLet
     print(f"Your encoded message is {newMsg}")
-# It's at this point that I'm going to veer off from
-# the guided path.  It's obvious the next phase will be to
-# set up Decryption and give the user the choice
-# So I'm going to go ahead and attempt that on my own :)
-
-def decrypt(userInput, shftAmt):
-    decodedMsg = ''
-    for letter in userInput:
-        letPos = alphabet.index(letter)
-        newPos = letPos - shftAmt
-        newLet = alphabet[newPos]
-        decodedMsg += newLet
-    print(f"Your DEcoded message is {decodedMsg}")
-decrypt(text, shift)
-input()
+# Hubris.  It's a b**** :)
+Caesar(direction,text, shift)
